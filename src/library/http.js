5ba5fd3ee16e3037ef2d0d0a1ab2181dd9d5e5c7
vsalfafara <alfafara.vm@gmail.com>
2023-09-15 14:58:15 +0800

    Moved api, key, and host in env

--- src/library/http.js
+++ src/library/http.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://mmo-games.p.rapidapi.com",
+  baseURL: import.meta.env.VITE_API,
   headers: {
-    "X-RapidAPI-Key": "b9f377de43msh10628aacb321df0p1be870jsn29f9c84e4616",
-    "X-RapidAPI-Host": "mmo-games.p.rapidapi.com",
+    "X-RapidAPI-Key": import.meta.env.VITE_KEY,
+    "X-RapidAPI-Host": import.meta.env.VITE_HOST,
   },
 });
 
